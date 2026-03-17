b0aee0fad3aab6c285e9f9b34319880eed6d8fe2
arif-fahrizal <fahrizalarif07@gmail.com>
2026-03-17 09:36:03 +0700

    [FEAT] Implement responsive design in Header

--- src/components/Layouts/Header.tsx
+++ src/components/Layouts/Header.tsx
@@ -14,19 +14,19 @@ export default function Header() {
     signOut();
   };
   return (
-    <header className="container fixed top-0 left-0 right-0 mx-auto px-4 py-6 z-99">
+    <header className="container sticky top-0 left-0 right-0 mx-auto px-4 py-6 z-99">
       <div className="flex justify-between items-center">
         <Link to="/" className="flex items-center gap-2">
-          <div className="flex justify-center items-center w-11 h-11 rounded-lg bg-linear-to-br from-purple-500 to-pink-500 overflow-hidden">
+          <div className="flex justify-center items-center w-9 h-9 md:w-12 md:h-12 rounded-lg bg-linear-to-br from-purple-500 to-pink-500 overflow-hidden">
             <img src="./images/brain-bolt-white.png" alt="" className="w-full h-full object-cover" />
           </div>
-          <h1 className="text-2xl text-white font-bold">BrainBolt</h1>
+          <h1 className="text-xl md:text-2xl text-white font-bold">BrainBolt</h1>
         </Link>
         <div className="flex items-center text-white gap-5">
           {user.status ? (
             <>
-              <span className="">{user.username}</span>
-              <button onClick={handleSignOut} className="btn-primary px-6 py-2">
+              <span className="hidden md:inline-block">{user.username}</span>
+              <button onClick={handleSignOut} className="btn-primary px-4 py-1.5 md:px-6 md:py-2 text-sm md:text-base">
                 Logout
               </button>
             </>
